588d3b04a2c0ff0c2b6b4bd86de02904dfe1e12f
Nashipae <kinyanjuiivy1@gmail.com>
2025-11-24 16:04:51 +0300

    commented out the blueprint code in app/init.py, to be able to run flask

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,9 @@ from .extensions import db
 from .config import Config
 
 # Import and register blueprints here
-from .routes import user_bp
-from .routes import report_bp
-from .routes import message_bp
+# from .routes import user_bp
+# from .routes import report_bp
+# from .routes import message_bp
 
 def create_app():
     app =  Flask(__name__)
@@ -13,9 +13,9 @@ def create_app():
     db.init_app(app)
 
 
-    app.register_blueprint(user_bp, url_prefix='/users')
-    app.register_blueprint(report_bp, url_prefix='/reports')
-    app.register_blueprint(message_bp, url_prefix='/messages')
+    # app.register_blueprint(user_bp, url_prefix='/users')
+    # app.register_blueprint(report_bp, url_prefix='/reports')
+    # app.register_blueprint(message_bp, url_prefix='/messages')
     
     return app
 
